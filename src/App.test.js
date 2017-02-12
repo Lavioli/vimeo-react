c736d6203496baa990aeb7d2009777986a510905
Lavie Ruan <lavieruan@Lavies-MacBook.local>
2017-02-12 03:59:50 -0600

    implement tests for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactTestUtils from 'react-addons-test-utils'
+import ReactTestUtils from 'react-addons-test-utils';
+import chai, { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
+import sinon from 'sinon';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import chai from 'chai';
-import {expect} from 'chai';
 import App from './components/App';
 import Video from './components/Video';
 
@@ -17,15 +17,18 @@ it('renders without crashing', () => {
 	  </Provider>, div);
 });
 
-let component,
-	wrapper;
-
+let component;
 
 describe('<App />', () => {
 	beforeEach(() => {
-	   component = mount(<App store={store} />); //mounts children as well
-	   wrapper = shallow(<App store={store} /> ); //no children involved
+		component = mount(<App store={store} />);
 	 });
+
+	it('should call component', () => {
+		sinon.spy(App.prototype, 'componentDidMount');
+		const wrapper = mount(<App store={store} />);
+		expect(App.prototype.componentDidMount.calledOnce).to.equal(true);
+	})
 
 	it('should render the header logo text', () => {
 	  const header = <header id="logo">React Vimeo</header>;
@@ -42,7 +45,15 @@ describe('<App />', () => {
 
 	it('should render 20 Video components from the json data', () => {
 		expect(component.find('Video')).to.have.length(20);
-	})
+	});
 
+	it('should props being passed down', () => {
+		const wrapper = mount(<App video='video' store={store} />);
+		expect(wrapper.prop('video')).to.equal('video');
+		expect(wrapper.get(0).selector.props.channelData).to.have.length(20);
+		expect(wrapper.get(0).selector.props.comments).to.equal(null);
+		expect(wrapper.get(0).selector.props.error).to.equal(null);
+	});
+	
 });
 
